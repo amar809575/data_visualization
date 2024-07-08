6cd92a41b2f85f2628149599be74b7c50888ae65
Amar Singh <81478859+amar809575@users.noreply.github.com>
2024-07-08 23:19:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'YOUR_MONGODB_URI',
+        target: 'http://localhost:5000',
         changeOrigin: true,
       },
     },
